a2c6de3ff945cb150fc387f2e6a78c35e71580d4
Tomas levy <tomas.levy@wearetonica.com>
2025-09-29 15:46:34 -0300

    Fix: footer page width

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { Phone, Mail } from "lucide-react";
 export const Footer = () => {
   return (
     <footer className="bg-black text-white text-sm py-8">
-      <div className="flex flex-col md:flex-row justify-between px-4 md:px-0 max-w-7xl mx-auto">
+      <div className="flex flex-col md:flex-row justify-between max-w-7xl mx-auto w-full max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
         <div className="mb-6 md:mb-0">
           <img
             src="/images/logo-white.png"
