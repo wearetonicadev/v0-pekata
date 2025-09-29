e085e1ead90139300acd3eca9fbe987e1dd943f9
Tomas levy <tomas.levy@wearetonica.com>
2025-09-29 20:58:25 -0300

    Fix: font sizes and colors

--- app/components/PendingTasks.tsx
+++ app/components/PendingTasks.tsx
@@ -10,7 +10,7 @@ interface PendingTasksProps {
 
 export const PendingTasks = ({ tasks }: PendingTasksProps) => {
   return (
-    <div className="bg-[#FFF5F5] rounded-lg p-6">
+    <div className="bg-[#FFF1F1] rounded-lg p-6">
       <div className="flex flex-col md:flex-row  md:items-center justify-between gap-4">
         <h3 className="text-xl text-black font-family-apercu font-semibold">
           Peticiones pendientes
@@ -22,15 +22,15 @@ export const PendingTasks = ({ tasks }: PendingTasksProps) => {
               key={index}
               className="bg-white rounded-lg p-4 shadow-sm border border-gray-100 w-full"
             >
-              <div className="text-red-500 text-sm font-medium mb-1">
+              <div className="text-[#BF0000] text-sm font-normal mb-1">
                 {task.title}
               </div>
 
               <div className="flex flex-row items-center gap-2">
-                <div className="text-black text-2xl font-semibold mb-1">
+                <div className="text-black text-2xl font-medium mb-1">
                   {task.value.toLocaleString()}
                 </div>
-                <div className="text-gray-500 text-sm font-semibold">
+                <div className="text-[#404040] text-sm font-normal">
                   {task.label}
                 </div>
               </div>
