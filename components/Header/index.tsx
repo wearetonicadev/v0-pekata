701264187123bc054c5cdd83c0f2776776106702
Tomas levy <tomas.levy@wearetonica.com>
2025-09-30 15:55:59 -0300

    Fix: header size and components

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -22,7 +22,7 @@ export const Header = () => {
   };
 
   return (
-    <header className="bg-white py-4">
+    <header className="bg-white py-8">
       <div className="flex items-center justify-between max-w-7xl px-4 md:px-6 lg:px-8 mx-auto">
         <CampaignLink href="/">
           <img src="/images/logo-black.png" alt="Pekata" className="w-1/3" />
@@ -31,12 +31,12 @@ export const Header = () => {
         <div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <div className="flex items-center space-x-2 cursor-pointer border border-[#D9E2EE] p-2 rounded-full">
-                <div className="bg-[#ECF1F6] rounded-full p-2">
+              <div className="flex items-center space-x-1 cursor-pointer border border-[#D9E2EE] p-1 rounded-full">
+                <div className="bg-[#ECF1F6] rounded-full p-1.5">
                   <User className="w-5 h-5 text-[#4370A8]" />
                 </div>
 
-                <ChevronDown className="w-4 h-4" />
+                <ChevronDown className="w-5 h-4" />
               </div>
             </DropdownMenuTrigger>
 
