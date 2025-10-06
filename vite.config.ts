8c9a88b901eebbe8aa70094f19a5f561ecce6cbe
francosabolo <franco.sabolo@wearetonica.com>
2025-10-06 15:35:05 -0300

    Vite confi minify

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    cssMinify: "lightningcss",
     rollupOptions: {
       output: {
         manualChunks: {
