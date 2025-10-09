95fe02170fff2c0232da1668f823c898c0a9502c
Tomas levy <tomas.levy@wearetonica.com>
2025-10-09 14:25:01 -0300

    Fix: employee details styling

--- src/components/EmployeeProfile.tsx
+++ src/components/EmployeeProfile.tsx
@@ -1,4 +1,4 @@
-import { User } from "lucide-react";
+import { CircleUserRound } from "lucide-react";
 import { CampaignUserDetail } from "@/types/campaigns";
 
 type EmployeeProfileProps = {
@@ -9,32 +9,32 @@ export const EmployeeProfile = ({ user }: EmployeeProfileProps) => {
   return (
     <div className="lg:col-span-1">
       <div className="bg-white rounded-lg border border-[#F1F1F4] p-6">
-        <div className="text-center mb-2 flex flex-row items-center md:flex-col">
-          <div className="w-16 h-16 bg-[#f1f1f4] rounded-full flex items-center justify-center mr-6 md:mr-0 md:mb-4">
-            <User className="w-8 h-8 text-neutral-700" />
+        <div className="text-center mb-5 flex flex-row items-center md:flex-col">
+          <div className="w-15 h-15 bg-[#F7FAF8] border border-[#D5EADE] rounded-full flex items-center justify-center mr-6 md:mr-0 md:mb-4">
+            <CircleUserRound className="w-6 h-6 text-[#1F503B]" />
           </div>
 
           <div className="flex flex-col">
-            <h2 className="text-lg font-semibold text-black mb-1">
+            <h2 className="text-lg font-semibold text-black mb-0.5">
               {user.first_name} {user.last_name}
             </h2>
-            <p className="text-sm text-neutral-700">{user.email}</p>
+            <p className="text-sm text-[#4D4D4D]">{user.email}</p>
           </div>
         </div>
 
-        <div className="space-y-4">
+        <div className="space-y-3">
           <div className="flex items-center justify-between">
-            <div className="text-sm font-medium text-neutral-600">Teléfono</div>
-            <div>{user.phone_number || "-"}</div>
+            <div className="text-sm font-normal text-[#666666]">Teléfono</div>
+            <div className="text-sm font-normal text-[#000000]">{user.phone_number || "-"}</div>
           </div>
 
-          <div className="border-b border-gray-300"></div>
+          <div className="border-b border-[#DBDFE9]"></div>
 
           <div className="flex items-center justify-between">
-            <div className="text-sm font-medium text-neutral-600">
+            <div className="text-sm font-normal text-[#666666]">
               External ID
             </div>
-            <div>{user.id || "-"}</div>
+            <div className="text-sm font-normal text-[#000000]">{user.id || "-"}</div>
           </div>
         </div>
       </div>
