3552b4be53a05dbe2275efde1a68bcf2f1ed0d8f
Tomas levy <tomas.levy@wearetonica.com>
2025-10-23 15:04:17 -0300

    Better login layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,16 +5,16 @@ export default function LoginPage() {
   return (
     <div className="flex flex-col min-h-screen md:flex-row">
       <div
-        className="flex-1 flex items-center justify-center bg-center bg-cover"
+        className="flex min-h-[300px] md:w-1/2 items-center justify-center bg-center bg-cover"
         style={{ backgroundImage: `url(./images/login-background.png)` }}
       >
         <img src="./images/logo-white.png" alt="Pekata" className="max-w-1/3" />
       </div>
 
-      <div className="flex flex-col md:items-center  md:justify-end flex-1/2 md:flex-1 p-8">
+      <div className="flex flex-col md:w-1/2 md:ml-1 lg:ml-20 md:items-start  md:justify-end flex-1/2 md:flex-1 p-8">
         <div className="flex-1 hidden md:block" />
 
-        <div className="md:flex-1 w-full md:max-w-3/4">
+        <div className="md:flex-1 w-full md:w-3/4 lg:w-3/5 md:max-w-[400px]">
           <h2 className="md:mt-6 text-[22px] md:text-[36px] font-bold mb-4">
             Bienvenidos a Pekata
           </h2>
@@ -22,7 +22,7 @@ export default function LoginPage() {
           <Form />
         </div>
 
-        <div className="flex flex-col gap-4 md:gap-1 text-sm md:flex-1 justify-end md:max-w-3/4 w-full mt-4 md:mt-0">
+        <div className="flex flex-col gap-4 md:gap-1 text-sm md:flex-1 justify-end md:max-w-3/4 w-full mt-4">
           <ContactInformation />
         </div>
       </div>
