581a047ec739b30152f3847a3a9a195c496fa677
Tomas levy <tomas.levy@wearetonica.com>
2025-11-05 19:56:50 -0300

    Fix: imports and apply filters button

--- src/components/EmployeesFilter/index.tsx
+++ src/components/EmployeesFilter/index.tsx
@@ -4,35 +4,15 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetClose } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetClose, SheetFooter } from "@/components/ui/sheet";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import {SlidersHorizontal} from "lucide-react";
 import { useState } from "react";
-import { Campaign } from "@/types/campaigns";
-import { useSearchParams } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "../ui/checkbox";
+import { FiltersData, FILTER_INFO   } from "@/types/filters";
+import {useAppliedFilters} from '@/hooks/use-applied-filters';
 
-export type FiltersData = {
-  work_centers: Campaign["work_centers"];
-  products: Campaign["predefined_lot_products"];
-  subsidiaries: Campaign["subsidiaries"];
-};
-
-export const FILTER_INFO: Record< keyof FiltersData, { label: string; urlParam: string }> = {
-  work_centers: { 
-    label: "Por Centros de trabajo", 
-    urlParam: "work_center" 
-  },
-  products: { 
-    label: "Por Productos", 
-    urlParam: "lot" 
-  },
-  subsidiaries: { 
-    label: "Por Subsidiarias", 
-    urlParam: "subsidiary" 
-  },
-}
 
 type FiltersProps = {
   isDisabled: boolean;
@@ -41,32 +21,12 @@ type FiltersProps = {
 
 export function EmployeesFilter({ filters, isDisabled }: FiltersProps) {
   const [open, setOpen] = useState(false);
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const toggleFilter = (key: keyof FiltersData, id: number | string) => {
-    const param = FILTER_INFO[key].urlParam;
-    const current = searchParams.get(param);
-
-    let newValue: string;
-    if (!current) {
-      newValue = String(id);
-    } else {
-      const values = current.split(",");
-      if (values.includes(String(id))) {
-        newValue = values.filter(v => v !== String(id)).join(",");
-      } else {
-
-        newValue = [...values, String(id)].join(",");
-      }
-    }
-
-    if (newValue === "") {
-      searchParams.delete(param);
-    } else {
-      searchParams.set(param, newValue);
-    }
-    setSearchParams(searchParams);
-  };
+  const {
+    appliedFilters,
+    toggleFilter,
+    applyFilters,
+    resetFilters,
+  } = useAppliedFilters();
 
   return (
     <>
@@ -85,60 +45,89 @@ export function EmployeesFilter({ filters, isDisabled }: FiltersProps) {
       </Button>
 
       {filters && (
-        <Sheet open={open} onOpenChange={setOpen}>
+        <Sheet
+        open={open}
+        onOpenChange={(isOpen) => {
+          setOpen(isOpen);
+          if (!isOpen) {
+            setTimeout(() => {
+              resetFilters();
+            }, 300);
+          }
+        }}
+      >
           <SheetContent side="right" className="sm:max-w-md">
             <SheetHeader>
               <SheetTitle>Filtrar:</SheetTitle>
             </SheetHeader>
-
-            <ScrollArea className=" h-[calc(100vh-8rem)] px-4">
+            <ScrollArea className=" h-[calc(100vh-8rem)] px-4">     
               {(Object.keys(filters) as Array<keyof FiltersData>).map((key) => {
                 const label = FILTER_INFO[key].label;
                 const param = FILTER_INFO[key].urlParam;
                 const items = filters[key] as Array<{ id: string | number; name: string }>;
 
-                const activeValues = searchParams.get(param)?.split(",") || [];
-                const isAccordionActive = items.some(item => activeValues.includes(String(item.id)));
-
-                if (items.length > 0) return (
-                  <Accordion key={key} defaultValue={isAccordionActive ? label : undefined} type="single" collapsible className="w-full bg-white border-1 rounded-lg border-gray-100 mt-2 first:mt-0">
-                    <AccordionItem value={label}  className="data-[state=open]:border-b-0">
-                      <AccordionTrigger className="px-4 py-2  h-[48px] hover:no-underline items-center  ">
-                        <h3 className="text-black font-normal">{label}</h3>
-
-                      </AccordionTrigger>
-                      <AccordionContent  overflowHidden className=" px-1 pb-2 flex flex-col gap-2">
-                        {items.map((item) => {
-                          const isActive = searchParams.get(param)?.split(",").includes(String(item.id));
-
-                          return (
-                            <button
-                              key={item.id}
-                              onClick={() => toggleFilter(key, item.id)}
-                              className={`text-left px-3 flex cursor-pointer hover:bg-gray-100 text-gray-800" items-center justify-start gap-3 py-1 rounded-md text-sm transition-colors
-                               `}
-                            >
-                              <Checkbox checked={isActive} />
-                              {item.name}
-                            </button>
-                          );
-                        })}
-                      </AccordionContent>
-                    </AccordionItem>
-                  </Accordion>
+                const activeValues = appliedFilters.get(param)?.split(",") || [];
+                const isAccordionActive = items.some(item =>
+                  activeValues.includes(String(item.id))
                 );
+
+                if (items.length > 0)
+                  return (
+                    <Accordion
+                      key={key}
+                      defaultValue={isAccordionActive ? label : undefined}
+                      type="single"
+                      collapsible
+                      className="w-full bg-white border-1 rounded-lg border-gray-100 mt-2 first:mt-0"
+                    >
+                      <AccordionItem
+                        value={label}
+                        className="data-[state=open]:border-b-0"
+                      >
+                        <AccordionTrigger className="px-4 py-2 h-[48px] hover:no-underline items-center">
+                          <h3 className="text-black font-normal">{label}</h3>
+                        </AccordionTrigger>
+
+                        <AccordionContent
+                          overflowHidden
+                          className="px-1 pb-2 flex flex-col gap-2"
+                        >
+                          {items.map((item) => {
+                            const isActive = activeValues.includes(String(item.id));
+
+                            return (
+                              <button
+                                key={item.id}
+                                onClick={() => toggleFilter(key, item.id)}
+                                className={`text-left px-3 flex cursor-pointer hover:bg-gray-100 text-gray-800 items-center justify-start gap-3 py-1 rounded-md text-sm transition-colors`}
+                              >
+                                <Checkbox checked={isActive} />
+                                {item.name}
+                              </button>
+                            );
+                          })}
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
+                  );
               })}
             </ScrollArea>
-
-            <SheetClose className="px-4">
-                  <Button
-                    variant="outline"
-                    size="sm"
-                    className="font-normal w-full px-2 flex items-center gap-1 border-black shadow-none text-xs"
-                  >
-                  <span className="block">Aplicar filtros</span>
-              </Button>
-            </SheetClose>
+            <SheetFooter>
+    
+                    <Button
+                      variant="outline"
+                      size="sm"
+                      className="font-normal w-full px-2 flex items-center gap-1 border-black shadow-none text-xs"
+                      onClick={() => {
+                        applyFilters();
+                        setOpen(false);
+                        }
+                      }
+                    >
+                    <span className="block">Aplicar filtros</span>
+                </Button>
+      
+            </SheetFooter>
           </SheetContent>
         </Sheet>
       )}
