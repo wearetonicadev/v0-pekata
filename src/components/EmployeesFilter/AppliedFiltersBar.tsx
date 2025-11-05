4dcaacfb7247b042a986a0d8634892e88648a295
Tomas levy <tomas.levy@wearetonica.com>
2025-11-05 19:45:27 -0300

    Fix: import

--- src/components/EmployeesFilter/AppliedFiltersBar.tsx
+++ src/components/EmployeesFilter/AppliedFiltersBar.tsx
@@ -1,5 +1,5 @@
 import { X } from "lucide-react";
-import { FILTER_INFO } from ".";
+import { FILTER_INFO } from "@/types/filters";
 import { Button } from "../ui/button";
 
 type AppliedFiltersBarProps = {
