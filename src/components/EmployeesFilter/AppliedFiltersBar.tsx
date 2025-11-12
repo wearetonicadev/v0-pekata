7cfd1e1f5a9faf91e4a85ebf639870c8dec3e2cb
Tomas levy <tomas.levy@wearetonica.com>
2025-11-12 19:08:19 -0300

    Fix: duplicated filter

--- src/components/EmployeesFilter/AppliedFiltersBar.tsx
+++ src/components/EmployeesFilter/AppliedFiltersBar.tsx
@@ -4,18 +4,54 @@ import { Button } from "../ui/button";
 
 type AppliedFiltersBarProps = {
   appliedFilters: Record<string, string>;
-  deleteUrlParam: (param: string) => void;
+  deleteUrlParam: (param: string | string[]) => void;
 };
 
 export function AppliedFiltersBar({ appliedFilters, deleteUrlParam }: AppliedFiltersBarProps){
+
+  const filteredAppliedFilters = Object.keys(appliedFilters).reduce<string[]>((filters, paramKey) => {
+    const filterInfo = Object.values(FILTER_INFO).find(
+      (info) => {
+        return info.urlParam === paramKey || paramKey.includes(info.urlParam);
+      }
+    );
+
+    if (!filterInfo) return filters;
+
+    if (filterInfo.type === "date" && paramKey.startsWith(`${filterInfo.urlParam}`)) {
+      if (!filters.includes(filterInfo.urlParam)) {
+        filters.push(filterInfo.urlParam);
+      }
+      return filters;
+    }
+
+    if (!filters.includes(paramKey)) {
+      filters.push(paramKey);
+    }
+
+    return filters;
+  }, []);
+
+  const handleDeleteFilter = (paramKey: string, filterEntry: { type: string }) => {
+    const paramsToDelete = filterEntry.type === 'date' 
+      ? ['close_date_from', 'close_date_to']
+      : paramKey;
+      
+    deleteUrlParam(paramsToDelete);
+  }
+
+
+
   return(
     <div className="flex gap-2 items-center flex-wrap">
           {(Object.keys(appliedFilters).length !== 0) && (
             <>
               Filtrando:
-              {Object.keys(appliedFilters).map((paramKey) => {
+              {filteredAppliedFilters.map((paramKey) => {
                 const filterEntry = Object.values(FILTER_INFO).find(
-                  (info) => info.urlParam === paramKey
+                   (info) =>  {
+                    return info.urlParam === paramKey ||  paramKey.includes(info.urlParam);
+                   } 
                 );
 
                 if (!filterEntry) return null;
@@ -24,7 +60,7 @@ export function AppliedFiltersBar({ appliedFilters, deleteUrlParam }: AppliedFil
                   <Button
                     key={paramKey}
                     variant="default"
-                    onClick={() => {deleteUrlParam(paramKey)}}
+                    onClick={() => { handleDeleteFilter(paramKey, filterEntry)}}
                     className="has-[>svg]:px-2 py-1 text-[10px] border-black rounded-md  h-5 "
                   >
                     {filterEntry.label}
